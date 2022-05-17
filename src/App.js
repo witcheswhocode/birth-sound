5d521f4f227a43ff587eabe7fac81dd250cfa53c
liz <lizanderson.ca@gmail.com>
2022-05-17 12:00:53 -0700

    basics of tone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import * as Tone from "tone";
+import { Sequence } from 'tone';
 
+const synth = new Tone.MembraneSynth().toMaster();
+function playSynth(){
+  const now = Tone.now()
+  synth.triggerAttack("D4", "8n", now);
+  synth.triggerAttack("F4", "8n", now + 0.5);
+  synth.triggerAttack("A4", "8n", now + 1);
+  synth.triggerAttack("C4", "8n", now + 1.5);
+  synth.triggerAttack("E4", "8n", now + 2);
+  //setTimeout(Tone.Transport.stop(),1000);
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <button id="button" onClick={playSynth}>Click Me</button>
     </div>
   );
 }
