056e0624ea800f0dbd677f0c6033add4cdebc701
liz <lizanderson.ca@gmail.com>
2022-05-26 14:21:37 -0700

    emphasize canvas and center

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Panel from './Panel';
 function App() {
   return (
     <div id="wrapper">
-      <Canvas />
+      <Canvas style={{
+          boxShadow:
+            "0 13px 27px -5px rgba(50, 50, 93, 0.25),    0 8px 16px -8px rgba(0, 0, 0, 0.3)"
+        }} />
       <Panel />
     </div>
   );
