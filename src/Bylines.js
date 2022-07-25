b25065e8274fe62cfccd01c2d467b445237c6974
liz <lizanderson.ca@gmail.com>
2022-07-24 21:00:11 -0700

    Update Bylines.js

--- src/Bylines.js
+++ src/Bylines.js
@@ -4,19 +4,19 @@ export const Bylines = () => {
     return (
       <footer>
       <div className='bylines-div'>
-        <img src='/khrystoph.jpg' alt='liz' />
-        <div className='bylines-text-div'>
-            <p><strong>Khrystoph</strong></p>
-            <p>Musician</p>
-            <p>email@address.com</p>
-        </div>
-      </div>
-      <div className='bylines-div'>
         <img src='/portrait1cropped.png' alt='liz' />
         <div className='bylines-text-div'>
             <p><strong>Liz Anderson</strong></p>
             <p>Indie Developer</p>
             <p>Website</p>
+        </div>
+      </div>
+      <div className='bylines-div'>
+        <img src='/khrystoph.jpg' alt='liz' />
+        <div className='bylines-text-div'>
+            <p><strong>Khrystoph</strong></p>
+            <p>Musician</p>
+            <p>email@address.com</p>
         </div>
       </div>
       </footer>
