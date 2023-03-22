2cb1f1324252c9a83a8eb499be33b5d425f287fa
Liz Anderson <lizanderson.ca@gmail.com>
2023-03-21 20:01:42 -0700

    Update dateToBirthchart.js

--- client/src/utils/dateToBirthchart.js
+++ client/src/utils/dateToBirthchart.js
@@ -2,7 +2,7 @@
 import Moment from 'moment';
 import { apiSignOrder, currentBirthChart } from '../data/settings';
 var ts = require('@mapbox/timespace');
-var host = 'http://localhost'
+var host = 'http://127.0.0.1'
 
 export async function nowToBirthchart(){
     const date = Moment().format('YYYY-MM-DD HH:mm:00');
