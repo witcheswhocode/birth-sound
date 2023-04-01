9a6da61687701d8e4db205ad46861a5048878ee8
Liz Anderson <lizanderson.ca@gmail.com>
2023-03-31 19:33:46 -0700

    Edit bylines, clean up

--- client/src/Bylines.js
+++ client/src/Bylines.js
@@ -5,18 +5,12 @@ export const Bylines = () => {
       <footer>
       <h3 className='section-header'>Brought to you by:</h3>
       <div className='bylines-div'>
-        <img src='/portrait1cropped.png' alt='liz' />
         <div className='bylines-text-div'>
-            <p><strong>Liz Anderson</strong></p>
-            <p>Indie Developer</p>
-            <p>Website</p>
-        </div>
-      </div>
-      <div className='bylines-div'>
-        <div className='bylines-text-div'>
+            <p>Design & code by <a href="https://github.com/witcheswhocode">Liz Anderson</a></p>
+            <br></br>
             <p><a href="https://github.com/ryuphi/astrology-api">Astrology API Code</a> by <a href="https://github.com/ryuphi">Marcos Corona</a></p>
             <br></br>
-            <p><a href="https://github.com/ryuphi/astrology-api">Give feedback here.</a></p>
+            <p><a href="">Give feedback here.</a></p>
         </div>
       </div>
       </footer>
