6bbf960f6fe46f531c88e2dc4fcb23ba651062aa
Liz Anderson <lizanderson.ca@gmail.com>
2023-04-03 15:54:46 -0700

    https

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 sudo apt-get update
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt install docker.io
+sudo apt install docker-compose
+sudo gpasswd -a $USER docker
+newgrp docker
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo apt install docker-compose
-sudo chmod 666 /var/run/docker.sock
+
+sudo service docker start
+
+npx browserslist@latest --update-db
