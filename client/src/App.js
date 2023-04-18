4c3fd284cd90edd303d23c2e780ca3ac9f5f51d9
Liz Anderson <lizanderson.ca@gmail.com>
2023-04-17 21:42:22 -0700

    Edit copy and some css

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,10 @@ function App() {
     <div id="wrapper">
       <div id="title"><h1>Birth Sound</h1></div>
       <EmojiLinebreak />
-      <p id='intro-p'>Create your personalized sound birth chart (natal chart) by filling in the form below. 
-      You can listen to your birth sound as if the signs represented the music theory wheel as explained in this TikTok by corruptcatalyst.</p>
+      <p id='intro-p'>Create your personalized sound birth chart (natal chart) by filling in the form below with your birth details. 
+      You can listen to your birth sound as if the signs represented the music theory wheel as explained in this <a href="https://www.tiktok.com/@corruptcatalyst/video/7041344969138785542">TikTok by corruptcatalyst</a>.</p>
+      <br></br>
+      <p id='disclaimer-p'>This is still a work in progress and a portfolio piece to present my creative abilities. This is best presented on desktop.</p>
       <EmojiLinebreak />
       <Canvas />
       <EmojiLinebreak />
